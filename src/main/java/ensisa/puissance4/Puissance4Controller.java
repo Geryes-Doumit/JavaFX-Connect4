6590c67f04f84905548272a11da42e9e8b0d7e4a
Marc Proux <prouxmarc@gmail.com>
2023-12-18 18:32:50 +0100

    modif du print de debug

--- src/main/java/ensisa/puissance4/Puissance4Controller.java
+++ src/main/java/ensisa/puissance4/Puissance4Controller.java
@@ -75,13 +75,14 @@ public class Puissance4Controller {
 
         // print grid for debugging
         int[][] modelGrid = model.getGrid();
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i<6; i++){
             System.out.print("|");
-            for (int j = 0; j < 6; j++) {
-                System.out.print(modelGrid[i][j] + "|");
+            for (int j = 0; j<7; j++){
+                System.out.print(modelGrid[j][5-i] + "|" );
             }
             System.out.println();
         }
+        System.out.println("-------");
 
         // updateView();
     }
